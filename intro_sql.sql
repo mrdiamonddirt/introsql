42915a4b6c0e416c69ddb4b2797995c944961531
RowDog <mrdiamonddirt@gmail.com>
2022-11-14 11:37:09 +0000

    ✅add update function

--- intro_sql.sql
+++ intro_sql.sql
@@ -4,21 +4,23 @@ SHOW TABLES;
 
 DROP TABLE movies;
 CREATE TABLE movies (
+	id int NOT NULL,
 	title VARCHAR(255) NOT NULL UNIQUE,
     actor VARCHAR(255) default "not specified",
     director VARCHAR(255) DEFAULT "not specified"
+    -- dateadded date DEFAULT CURRENT_DATE() 
 	);
 
 -- INSERT SPIDERMAN MOVIES INTO DATABASE
 INSERT INTO movies VALUES 
-	("SPIDERMAN","Tobey Maguire","sam raimi"),
-	("SPIDERMAN 2","Tobey Maguire","sam raimi"),
-	("SPIDERMAN 3","Tobey Maguire","sam raimi"),
-	("The Amazing Spiderman","Andrew Garfield","Marc Webb"),
-	("The Amazing Spiderman 2","Andrew Garfield","Marc Webb"),
-	("Spider-Man: Homecoming","Tom Holland","Jon Watts"),
-	("Spider-Man: Far From Home", "Tom Holland","Jon Watts"),
-	("Spider-Man: No Way Home", "Tom Holland" , "Jon Watts");
+	(1,"SPIDERMAN","Tobey Maguire","sam raimi"),
+	(2,"SPIDERMAN 2","Tobey Maguire","sam raimi"),
+	(3,"SPIDERMAN 3","Tobey Maguire","sam raimi"),
+	(4,"The Amazing Spiderman","Andrew Garfield","Marc Webb"),
+	(5,"The Amazing Spiderman 2","Andrew Garfield","Marc Webb"),
+	(6,"Spider-Man: Homecoming","Tom Holland","Jon Watts"),
+	(7,"Spider-Man: Far From Home", "Tom Holland","Jon Watts"),
+	(8,"Spider-Man: No Way Home", "Tom Holland" , "Jon Watts");
 
 
 -- INSERT INTO `bdgtz9j1xtvceikhuc8p`.`movies` (`title`, `actor`, `director`) VALUES ('spiderman 2', 'Tobey Maguire', 'sam raimi');
@@ -26,3 +28,7 @@ INSERT INTO movies VALUES
 SELECT * FROM movies;
 
 -- reaarch the UPDATE and DELETE command, amd add 4 or 5 movies update director on one and delete atleast one row
+-- update function
+UPDATE movies
+SET actor = 'Tobey Maguirei'
+WHERE director = "sam raimi";

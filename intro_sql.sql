d54c53790247b5cb51d3538a753a99966d4bc864
RowDog <mrdiamonddirt@gmail.com>
2022-11-14 15:00:03 +0000

    🔢Auto increment

--- intro_sql.sql
+++ intro_sql.sql
@@ -4,23 +4,28 @@ SHOW TABLES;
 
 DROP TABLE movies;
 CREATE TABLE movies (
-	id int NOT NULL,
 	title VARCHAR(255) NOT NULL UNIQUE,
     actor VARCHAR(255) default "not specified",
     director VARCHAR(255) DEFAULT "not specified"
     -- dateadded date DEFAULT CURRENT_DATE() 
 	);
 
+-- ALTER table
+ALTER TABLE movies
+ADD movie_id INT unique NOT NULL PRIMARY KEY AUTO_INCREMENT;
+
+insert INTO movies (title,actor,director) VALUES("spiderman 2", "toby magguire","sam raimi");
+
 -- INSERT SPIDERMAN MOVIES INTO DATABASE
 INSERT INTO movies VALUES 
-	(1,"SPIDERMAN","Tobey Maguire","sam raimi"),
-	(2,"SPIDERMAN 2","Tobey Maguire","sam raimi"),
-	(3,"SPIDERMAN 3","Tobey Maguire","sam raimi"),
-	(4,"The Amazing Spiderman","","Marc Webb"),
-	(5,"The Amazing Spiderman 2","","Marc Webb"),
-	(6,"Spider-Man: Homecoming","Tom Holland","Jon Watts"),
-	(7,"Spider-Man: Far From Home", "Tom Holland","Jon Watts"),
-	(8,"Spider-Man: No Way Home", "Tom Holland" , "Jon Watts");
+	("SPIDERMAN","Tobey Maguire","sam raimi"),
+	("SPIDERMAN 2","Tobey Maguire","sam raimi"),
+	("SPIDERMAN 3","Tobey Maguire","sam raimi"),
+	("The Amazing Spiderman","","Marc Webb"),
+	("The Amazing Spiderman 2","","Marc Webb"),
+	("Spider-Man: Homecoming","Tom Holland","Jon Watts"),
+	("Spider-Man: Far From Home", "Tom Holland","Jon Watts"),
+	("Spider-Man: No Way Home", "Tom Holland" , "Jon Watts");
 
 
 -- INSERT INTO `bdgtz9j1xtvceikhuc8p`.`movies` (`title`, `actor`, `director`) VALUES ('spiderman 2', 'Tobey Maguire', 'sam raimi');
